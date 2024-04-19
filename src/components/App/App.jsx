8c1c4535c7cb48e1875c3f8758dfb142059b0193
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-04-19 03:48:47 +0300

    Added to the project ACCESS_KEY

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import { fetchImages } from "../../images-api";
+import css from "./App.module.css";
+// import ImageGallery from "../ImageGallery/ImageGallery";
+
 export default function App() {
+  // const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    async function getImages() {
+      const imageData = await fetchImages();
+      console.log(imageData);
+      // setImages(imageData);
+    }
+
+    getImages();
+  }, []);
+
   return (
-    <>
-      <h1>Test goit-react-hw-04</h1>
-    </>
+    <div className={css.container}>
+      <h1>Image search application by keyword</h1>
+      {/* <ImageGallery images={images} /> */}
+    </div>
   );
 }
---
+++ src/images-api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.unsplash.com/";
+
+const ACCESS_KEY = "jUX8RIPi9BwKL9E9VSwgkiB037_bbGeNpple5xrBBWY";
+
+export const fetchImages = async () => {
+  const response = await axios.get(`photos/?client_id=${ACCESS_KEY}`);
+  return response.imageData;
+};
